bce30d2fe0f66e0ad24ffe659b2092daf8635637
txu1 <txu1@i9i8.com>
2020-10-26 09:08:43 +0800

    添加我的订房相关接口

--- foodie-dev-service/src/main/java/com/monkey/service/impl/center/MyOrdersServiceImpl.java
+++ foodie-dev-service/src/main/java/com/monkey/service/impl/center/MyOrdersServiceImpl.java
@@ -2,15 +2,23 @@ package com.monkey.service.impl.center;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.monkey.enums.OrderStatusEnum;
+import com.monkey.enums.YesOrNo;
+import com.monkey.mapper.OrderStatusMapper;
+import com.monkey.mapper.OrdersMapper;
 import com.monkey.mapper.OrdersMapperCustom;
+import com.monkey.pojo.OrderStatus;
+import com.monkey.pojo.Orders;
 import com.monkey.pojo.vo.MyOrdersVO;
 import com.monkey.service.center.MyOrdersService;
 import com.monkey.utils.PagedGridResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,6 +32,12 @@ public class MyOrdersServiceImpl implements MyOrdersService {
 
     @Resource
     private OrdersMapperCustom ordersMapperCustom;
+
+    @Resource
+    private OrderStatusMapper orderStatusMapper;
+
+    @Resource
+    private OrdersMapper ordersMapper;
 
     @Transactional(propagation = Propagation.SUPPORTS, rollbackFor = Exception.class)
     @Override
@@ -49,5 +63,65 @@ public class MyOrdersServiceImpl implements MyOrdersService {
         pagedGridResult.setTotal(pageList.getPages());
         pagedGridResult.setRecords(pageList.getTotal());
         return pagedGridResult;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Override
+    public void updateDeliverOrderStatus(String orderId) {
+        OrderStatus updateOrder = new OrderStatus();
+        updateOrder.setOrderStatus(OrderStatusEnum.WAIT_RECEIVE.type);
+        updateOrder.setDeliverTime(new Date());
+
+        Example example = new Example(OrderStatus.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("orderId", orderId);
+        criteria.andEqualTo("orderStatus", OrderStatusEnum.WAIT_DELIVER.type);
+
+        orderStatusMapper.updateByExampleSelective(updateOrder, example);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS, rollbackFor = Exception.class)
+    @Override
+    public Orders queryMyOrder(String userId, String orderId) {
+
+        Orders orders = new Orders();
+        orders.setUserId(userId);
+        orders.setId(orderId);
+        orders.setIsDelete(YesOrNo.NO.type);
+
+        return ordersMapper.selectOne(orders);
+    }
+
+    @Override
+    public boolean updateReceiveOrderStatus(String orderId) {
+        OrderStatus orderStatus = new OrderStatus();
+        orderStatus.setOrderStatus(OrderStatusEnum.SUCCESS.type);
+        orderStatus.setSuccessTime(new Date());
+
+        Example example = new Example(OrderStatus.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("orderId", orderId);
+        criteria.andEqualTo("orderStatus", OrderStatusEnum.WAIT_RECEIVE.type);
+
+        int result = orderStatusMapper.updateByExampleSelective(orderStatus, example);
+
+        return result == 1;
+    }
+
+    @Override
+    public boolean deleteOrder(String userId, String orderId) {
+
+        Orders order = new Orders();
+        order.setIsDelete(YesOrNo.YES.type);
+        order.setUpdatedTime(new Date());
+
+        Example example = new Example(Orders.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("orderId", orderId);
+        criteria.andEqualTo("userId", userId);
+
+        int result = ordersMapper.updateByExampleSelective(order, example);
+
+        return result == 1;
     }
 }
